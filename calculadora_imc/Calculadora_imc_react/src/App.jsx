35ec9f2da8f882416027ab03c64cf4503caf323e
Henrique-ikeda <riqueikeda2004@gmail.com>
2025-07-10 10:07:11 -0300

    iniciando a estrutura

---
+++ calculadora_imc/Calculadora_imc_react/src/App.jsx
@@ -0,0 +1,15 @@
+import ImcCalc from './components/imcCalc'
+
+import './App.css'
+
+
+function App() {
+
+  return (
+      <div className="container">
+        <ImcCalc/>
+      </div>
+  )
+}
+
+export default App
